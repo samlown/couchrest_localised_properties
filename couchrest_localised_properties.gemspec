6bae62f7e81f141a022d50b99993c7990c5e9c79
Sam Lown <me@samlown.com>
2011-03-24 09:49:00 +0100

    First working version

--- couchrest_localised_properties.gemspec
+++ couchrest_localised_properties.gemspec
@@ -1,28 +1,27 @@
 # -*- encoding: utf-8 -*-
 
 Gem::Specification.new do |s|
-  s.name = %q{translate_columns}
+  s.name = %q{couchrest_localised_properties}
   s.version = File.read(File.join(File.dirname(__FILE__), 'VERSION')).strip
 
   s.required_rubygems_version = Gem::Requirement.new("> 1.3.5") if s.respond_to? :required_rubygems_version=
   s.authors = ["Sam Lown"]
-  s.date = %q{2011-01-21}
-  s.description = %q{Automatically translate ActiveRecord columns using a second model containing the translations.}
+  s.date = %q{2011-03-23}
+  s.description = %q{Add support to CouchRest Model for localised properties.}
   s.email = %q{me@samlown.com}
   s.extra_rdoc_files = [
     "LICENSE",
     "README.rdoc"
   ]
-  s.homepage = %q{http://github.com/samlown/translate_columns}
+  s.homepage = %q{https://github.com/samlown/couchrest_localised_properties}
   s.rubygems_version = %q{1.3.7}
-  s.summary = %q{Use fields from other translation models easily}
+  s.summary = %q{Add support to CouchRest Model for localised properties.}
   
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.require_paths = ["lib"]
   
-  s.add_dependency("activerecord", "~> 3.0.0")
-  s.add_development_dependency(%q<mocha>, "~> 0.9.3")
-  s.add_development_dependency(%q<sqlite3>, "~> 1.3.3")
+  s.add_dependency("couchrest_model", "~> 1.1.0.beta")
+  s.add_development_dependency(%q<rspec>, "~> 1.2.0")
 end
 
--- lib/couchrest_localised_properties.rb
+++ lib/couchrest_localised_properties.rb
@@ -1,3 +1,79 @@
 #
+# CouchRest Localised Properties
 #
-#
+
+module CouchRest
+  module LocalisedProperties
+
+    extend ActiveSupport::Concern
+
+    def read_localised_attribute(property)
+      attr = (self[find_property!(property).to_s] || {})
+      attr[I18n.locale.to_s] || attr[I18n.default_locale.to_s]
+    end
+
+    def write_localised_attribute(property, value)
+      prop = find_property!(property)
+      key = prop.to_s
+      if value.is_a?(Hash) # from mass-asign
+        self[key] = value
+        self[key].each do |k,v|
+          self[key][k] = prop.cast(self, v)
+        end
+      else
+        self[key] ||= { }
+        self[key][I18n.locale.to_s] = prop.is_a?(String) ? value : prop.cast(self, value)
+      end
+    end
+
+    module ClassMethods
+
+      def localised_property(name, *options, &block)
+        # Create a normal property
+        p = property(name, *options, &block)
+        raise "Localised properties cannot be used with Hashes!" if p.type == Hash
+        # Override the normal methods with special accessors
+        create_localised_property_getter(p)
+        create_localised_property_setter(p)
+      end
+
+      protected
+
+      # defines the getter for the property (and optional aliases)
+      def create_localised_property_getter(property)
+        # meth = property.name
+        class_eval <<-EOS, __FILE__, __LINE__ + 1
+          def #{property.name}
+            read_localised_attribute('#{property.name}')
+          end
+        EOS
+        if ['boolean', TrueClass.to_s.downcase].include?(property.type.to_s.downcase)
+          class_eval <<-EOS, __FILE__, __LINE__
+            def #{property.name}?
+              value = read_localised_attribute('#{property.name}')
+              !(value.nil? || value == false)
+            end
+          EOS
+        end
+      end
+
+      # defines the setter for the property (and optional aliases)
+      def create_localised_property_setter(property)
+        property_name = property.name
+        class_eval <<-EOS
+          def #{property_name}=(value)
+            write_localised_attribute('#{property_name}', value)
+          end
+        EOS
+      end
+
+    end
+
+  end
+end
+
+# Add self to the CouchRest Model Base
+CouchRest::Model::Base.class_eval do
+  include CouchRest::LocalisedProperties
+end
+
